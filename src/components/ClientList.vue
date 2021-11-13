501a7e7db7f0f42e1638731b03f92f07709cad1a
Nehemiah Dacres <vivacarlie@gmail.com>
2021-11-13 17:10:42 -0600

    made it to empty client list
    layout done, views done

---
+++ src/components/ClientList.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <p> Client List </p>
+    <li :clients="c in clients">
+      {{c.name}} | {{c.ssn}}
+    </li>
+  </ul>
+<template>
+<script>
+export default {
+  name: 'ClientList',
+  props: {
+    clients: Array
+  }
+}
+</script>
+
+<!--
+ <ClientList clients=[
+   { name: nemo, ssn: 777-77-7777},
+   { name: dora, ssn: 999-99-9999}
+ ] >
+--> 
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <WelcomePage msg="Welcome to Open Tax Solver"/>
+    <ClientList clients= [] />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import WelcomePage from '@/components/WelcomePage.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    WelcomePage
+  }
+}
+</script>
