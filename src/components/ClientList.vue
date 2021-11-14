911480cd8e7691f2880d7f7b3cd4340ea78cb118
Nehemiah Dacres <vivacarlie@gmail.com>
2021-11-13 21:20:17 -0600

    got list working with proto data

--- src/components/ClientList.vue
+++ src/components/ClientList.vue
@@ -1,23 +1,21 @@
 <template>
   <ul>
     <p> Client List </p>
-    <li :clients="c in clients">
-      {{c.name}} | {{c.ssn}}
+    <li v-for="c in clients" :key=c.id>
+      {{ c.name }} : {{ c.ssn }}
     </li>
   </ul>
-<template>
+</template>
 <script>
 export default {
-  name: 'ClientList',
-  props: {
-    clients: Array
+  //name: 'ClientList',
+  data() {
+    return {
+      clients: [
+       { name: 'nemo', ssn: '777-77-7777'},
+       { name: 'dora', ssn: '999-99-9999'}
+     ] }
   }
 }
 </script>
-
-<!--
- <ClientList clients=[
-   { name: nemo, ssn: 777-77-7777},
-   { name: dora, ssn: 999-99-9999}
- ] >
---> 
+ 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <WelcomePage msg="Welcome to Open Tax Solver"/>
-    <ClientList clients= [] />
+    <ClientList  />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import WelcomePage from '@/components/WelcomePage.vue'
+import ClientList from '@/components/ClientList.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    WelcomePage
+    WelcomePage,
+    ClientList 
   }
 }
 </script>
